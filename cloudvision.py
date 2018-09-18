5bd3e05ed4a7088cd03ef1dfd45e408c56603c3a
Chester <chesta@kekt.us>
2018-09-18 17:20:26 +0200

    working label detection

---
+++ cloudvision.py
@@ -0,0 +1,26 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+class CloudVision:
+    client = vision.ImageAnnotatorClient()
+
+    def getLabels(self, image):
+        image = types.Image(content=image)
+        response = self.client.label_detection(image=image)
+        return response.label_annotations
+
+
+# ------------
+
+with io.open('igel.png', 'rb') as image_file:
+    content = image_file.read()
+
+vision = CloudVision()
+labels = vision.getLabels(content)
+
+for label in labels:
+    print(label.description)
