fb7ed6c40cef272fe8ca3b53174e6ab6deebda2e
Chester <chesta@kekt.us>
2018-09-18 18:52:01 +0200

    Adjusted for new config workings, added detectionTrigger

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import cv2
 import cloudvision
 import motiondetection
-import json
 
-config = json.load('config.json')
+from configloader import config
+
 detection = motiondetection.MotionDetection()
 vision = cloudvision.CloudVision()
+
+
+def onDetectionTrigger(frame):
+    print('detection!')
+    cv2.imshow("Detection", frame)
+    cv2.waitKey(0)
+
 
 def onMotionTrigger(frame):
     labels = vision.getLabels(frame)
     descriptions = []
     for label in labels:
         descriptions.append(label.description)
+        print(label.description)
 
-    
+    detecTrigger = False
+
+    # detect if the detected possible subjects are whitelisted
+    detecTrigger = config.mode == 'whitelist' and
+    set(descriptions).isdisjoint(config.whitelist)
+
+    # detect if any possible subjects are blacklisted
+    detecTrigger = config.mode == 'blacklist' and
+    not set(descriptions).isdisjoint(config.blacklist)
+
+    if detecTrigger:
+        onDetectionTrigger(frame)
 
 # add handler to trigger event
 detection.onTrigger += onMotionTrigger
